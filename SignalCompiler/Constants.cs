272854c51c9e86ccf5d69677d1535c530261d2c2
Podze Aleksandr <dasdeg@gmail.com>
2015-02-16 21:41:12 +0200

    constants class

---
+++ SignalCompiler/Constants.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SignalCompiler
+{
+    public static class Constants
+    {
+        public static IEnumerable<char> Letters;
+        public static IEnumerable<char> Digits;
+        public static IEnumerable<char> Delimiter;
+        public static IEnumerable<char> Whitespace;
+        public static IEnumerable<string> MultiSymbolDelimiter;
+        public static IEnumerable<string> Keywords;
+        public static IEnumerable<string> GlobalLexemTable;
+        public const string BegComment = "(*";
+        public const string EndComment = "*)";
+        private static IEnumerable<char> GenCharEnumerable(char start, char fin)
+        {
+            return Enumerable.Range(start, (fin - start) + 1).Select(c => (char)c);
+        }
+
+
+        static Constants()
+        {
+            Letters = GenCharEnumerable('A', 'Z');
+            Digits = GenCharEnumerable('0', '9');
+            Delimiter = new[]
+            {
+                '<', '>', '=', ';'
+            };
+            Whitespace = new[]
+            {
+                '\x9', //horizontal tab
+                '\xA', //LF
+                '\xB', //vertical tab
+                '\xC', //new page
+                '\xD', //CR
+                '\x20', //space
+            };
+            MultiSymbolDelimiter = new[]
+            {
+                "<=", ">=", "<>"
+            };
+            Keywords = new[]
+            {
+                "PROGRAM",
+                "BEGIN",
+                "END",
+                "IF",  "THEN",  "ENDIF",
+                "WHILE","DO", "ENDWHILE"
+            };
+
+            GlobalLexemTable = Letters.Select(x => x.ToString())
+                                    .Union(Digits.Select(x => x.ToString()))
+                                    .Union(Delimiter.Select(x => x.ToString()))
+                                    .Union(MultiSymbolDelimiter)
+                                    .Union(Keywords)
+                                    .OrderBy(x => x);
+        }
+    }
+}
---
+++ SignalCompiler/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = GetProgramCode(args);
+            if (filename == null)
+            {
+                return;
+            }
+            string code = File.ReadAllText(filename);
+        }
+
+        private static string GetProgramCode(string[] args)
+        {
+            string filename;
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Too many args");
+                return null;
+            }
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Enter filename");
+                filename = Console.ReadLine();
+            }
+            else
+            {
+                filename = args[0];
+            }
+
+            if (!File.Exists(filename))
+            {
+                Console.WriteLine("File doesn't exist!");
+                return null;
+            }
+            return filename;
+        }
+    }
+}
