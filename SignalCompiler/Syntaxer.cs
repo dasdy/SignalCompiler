1ed6f901a59c1a4caa597c7660c95499e256d56f
Podze Aleksandr <dasdeg@gmail.com>
2015-03-08 16:32:21 +0200

    dummy functions

--- SignalCompiler/Syntaxer.cs
+++ SignalCompiler/Syntaxer.cs
@@ -11,6 +11,71 @@ namespace SignalCompiler
     {
         public SyntaxTree Feed(int[] lexTable, IList<CompilerError> errors)
         {
+            return Program(lexTable,errors);
+        }
+
+        private SyntaxTree Program(int[] lexTable, IList<CompilerError> errors)
+        {
+            return null;
+        }
+
+        private SyntaxTree Block(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree Stmtlist(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree Stmt(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree CondStmt(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree ThenStmt(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree ElseStmt(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree CondExpr(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree ComparisonOp(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree Expression(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree VarIdentifier(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree ProcIdentifier(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
+            return null;
+        }
+
+        private SyntaxTree Identifier(int[] lexTable, IList<CompilerError> errors, int pos)
+        {
             return null;
         }
     }
